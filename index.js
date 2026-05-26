0d2ffe0d6bd0fe0a908d25e866d5fd39499d13fe
dqnx19 <dan.k.19@outlook.com>
2026-05-26 12:26:48 +0200

    updated css and web pages

--- index.js
+++ index.js
@@ -13,83 +13,86 @@ header.innerHTML = `
 function showHome() {
     document.title = "Nether Modern Web"
     main.innerHTML = `
-        <h1>Home Page</h1>
+         <h1>Home Page</h1>
         <section>
             <h2>Elements</h2>
             <h3>Base Structure</h3>
-            <div class="cards">
-                <button class="card" onclick="showCSSFileDetails('All', 'all', 'elements')">
+            <div class="grouped-list">
+                <button class="item" onclick="showCSSFileDetails('All', 'all', 'elements')">
                     <span>All elements</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('Body', 'body', 'elements')">
+                <button class="item" onclick="showCSSFileDetails('Body', 'body', 'elements')">
                     <span>Body</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('Header', 'header', 'elements')">
+                <button class="item" onclick="showCSSFileDetails('Header', 'header', 'elements')">
                     <span>Header</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('Main', 'main', 'elements')">
+                <button class="item" onclick="showCSSFileDetails('Main', 'main', 'elements')">
                     <span>Main</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('Footer', 'footer' , 'elements')">
+                <button class="item" onclick="showCSSFileDetails('Footer', 'footer' , 'elements')">
                     <span>Footer</span>
                 </button>
-                <button class="card">
+                <button class="item">
                     <span>Section</span>
                 </button>
             </div>
-            <h3>Text and Lists</h3>
             <div class="cards">
-                <button class="card" onclick="showCSSFileDetails('H1 (Heading 1)', 'h1', 'elements')">
+                
+            </div>
+            <h3>Text and Lists</h3>
+            <div class="grouped-list">
+                <button class="item" onclick="showCSSFileDetails('H1 (Heading 1)', 'h1', 'elements')">
                     <span>H1 (Heading 1)</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('H2 (Heading 1)', 'h2', 'elements')">
+                <button class="item" onclick="showCSSFileDetails('H2 (Heading 1)', 'h2', 'elements')">
                     <span>H2 (Heading 2)</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('P (Paragraph)', 'p', 'elements')">
+                <button class="item" onclick="showCSSFileDetails('P (Paragraph)', 'p', 'elements')">
                     <span>P (Paragraph)</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('A (Link)', 'a', 'elements')">
+                <button class="item" onclick="showCSSFileDetails('A (Link)', 'a', 'elements')">
                     <span>A (Link)</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('LI (List Iem)', 'li', 'elements')">
+                <button class="item" onclick="showCSSFileDetails('LI (List Iem)', 'li', 'elements')">
                     <span>LI (List Item)</span>
                 </button>
             </div>
             <h3>Images</h3>
-            <div class="cards">
-                <button class="card" onclick="showCSSFileDetails('Img (Image)', 'img', 'elements')">
+            <div class="grouped-list">
+                <button class="item" onclick="showCSSFileDetails('Img (Image)', 'img', 'elements')">
                     <span>Img (Image)</span>
                 </button>
             </div>
             <h3>Table</h3>
-            <div class="cards">
-                <button class="card">
+            <div class="grouped-list">
+                <button class="item">
                     <span>Table</span>
                 </button>
-                <button class="card">
+                <button class="item">
                     <span>TR (Table Row)</span>
                 </button>
             </div>
         </section>
         <section>
             <h2>Components</h2>
-            <div class="cards">
-                <button class="card" onclick="showCSSFileDetails('App Drawer', 'app-drawer', 'components')">
+            <div class="grouped-list">
+                <button class="item" onclick="showCSSFileDetails('App Drawer', 'app-drawer', 'components')">
                     <span>App Drawer</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('Cards', 'cards', 'components')">
+                <button class="item" onclick="showCSSFileDetails('Cards', 'cards', 'components')">
                     <span>Cards</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('Copy Box', 'copy-box', 'components')">
+                <button class="item" onclick="showCSSFileDetails('Copy Box', 'copy-box', 'components')">
                     <span>Copy Box</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('Footer Bar', 'footer-bar', 'components')">
+                <button class="item" onclick="showCSSFileDetails('Footer Bar', 'footer-bar', 'components')">
                     <span>Footer Bar</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('Logo', 'logo', 'components')">
+                <button class="item" onclick="showCSSFileDetails('Logo', 'logo', 'components')">
                     <span>Logo</span>
                 </button>
-                <button class="card" onclick="showCSSFileDetails('Services Icons', 'services-icons', 'components')">
+                <button class="item" onclick="showCSSFileDetails('Services Icons', 'services-icons', 'components')">
                     <span>Services Icons</span>
                 </button>
             </div>
@@ -103,7 +106,8 @@ function showCSSFileDetails(nameUpperCase, nameLowerCase, type) {
         <h1>${nameUpperCase}</h1>
         <section>
             <h2>CSS import</h2>
-            <p>Use one of the following methods to import the CSS file (do not use both of them):</p>
+            <li>Use one of the following methods to import the CSS file</li>
+            <li>Do not use both of them</li>
             <br>
             <div class="copy-box">
                 <div class="head">
@@ -136,7 +140,7 @@ function showCSSFileDetails(nameUpperCase, nameLowerCase, type) {
             </div>
         </section>
         <section>
-            <h2>JS</h2>
+            <h2>JS Import</h2>
             <div class="copy-box">
                 <div class="head">
                     <span class="language">HTML</span>
@@ -149,19 +153,6 @@ function showCSSFileDetails(nameUpperCase, nameLowerCase, type) {
                     <input class="code" type="text"
                         value="&lt;script src&quot;https://modern-web.nether.click/css/${type}/${nameLowerCase}.js&quot;&gt;"
                         readonly>
-                </div>
-            </div>
-            <br>
-            <div class="copy-box">
-                <div class="head">
-                    <span class="language">JS</span>
-                    <button class="copy-button">
-                        <img src="img/copy-icon.svg" alt="Copy Icon">
-                        Copy
-                    </button>
-                </div>
-                <div class="body">
-                    <pre class="code" id="js"></pre>
                 </div>
             </div>
         </section>
