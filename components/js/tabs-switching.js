bfea479a3b270699116314911d50d153e97a8171
dqnx19 <dan.k.19@outlook.com>
2026-06-24 14:30:14 +0200

    patch 2 for tabs switching

--- components/js/tabs-switching.js
+++ components/js/tabs-switching.js
@@ -1,19 +1,14 @@
-function showTab(id, button = null) {
-    document.querySelectorAll('.tab-content').forEach(content => {
-        content.classList.remove('active');
-    });
+function showTab(id) {
+    document.querySelectorAll('.tab-content').forEach(c =>
+        c.classList.remove('active')
+    );
 
-    document.querySelectorAll('.tab').forEach(tab => {
-        tab.classList.remove('active');
-    });
+    document.querySelectorAll('.tab').forEach(t =>
+        t.classList.remove('active')
+    );
 
     document.getElementById(id)?.classList.add('active');
 
-    if (button) {
-        button.classList.add('active');
-    } else {
-        document
-            .querySelector(`.tab[data-tab="${id}"]`)
-            ?.classList.add('active');
-    }
+    document.querySelector(`.tab[data-tab="${id}"]`)
+        ?.classList.add('active');
 }
